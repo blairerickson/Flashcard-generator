5767a7faf6cb50fd0e471fefd7af6e214899e349
Blair Erickson <blair@jamwix.com>
2017-04-05 23:52:48 -0400

    JSON question update

--- flashcard.js
+++ flashcard.js
@@ -7,6 +7,7 @@ var question = "Here is a question";
 
 var score = [0,0];
 
+// Our ClozeCard constructor. 
 function ClozeCard(text, cloze) {
     this.text = text;
     this.cloze = cloze;
@@ -28,11 +29,7 @@ function ClozeCard(text, cloze) {
 var jsonContent = fs.readFileSync("cards.json");
 // Define to JSON type
 var cards = JSON.parse(jsonContent);
-// test JSON reading
-// console.log("question1:", cards.data[1].q);
-// console.log("question1:", cards.data[1].cloze);
-// console.log("question1:", cards.data[1].front);
-// console.log("\n *EXIT* \n");
+
 
 
 
@@ -51,11 +48,23 @@ function quiz(){
             },
         ]).then(function (answers) {
             console.log("Answered:" + answers.useranswer);
+            if (answers.useranswer == cards.data[i].cloze)
+            {
+                score[0]++;
+                console.log("Correct! Score + 1 \n Current correct answers: " + score[0] )
+            }
+            else
+            {
+                score[1]++;
+                console.log("Sorry, " + cards.data[i].cloze + " is the right answer. \n Current incorrect answers: " + score[1] )
+            }
             i++;
             quiz();
         });
 
-    };
-
+    }
+    else{
+        console.log("\n Good game! \n Correct answers: " + score[0] + "\n Wrong answers:" + score[1]);
+    }
 };
 
